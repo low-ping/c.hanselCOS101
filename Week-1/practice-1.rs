e7ebff8011fe7855cf64644e8e1b414bf09f1b08
low-ping <148707881+low-ping@users.noreply.github.com>
2023-10-22 09:17:06 -0700

    My commit

---
+++ Week-1/practice-1.rs
@@ -0,0 +1,13 @@
+fn main(){
+    println!("Welcome to COS 102!");
+
+    println!("The course learning outcome is as follows:");
+
+     println!("1. Distinguish between computer and computer programming,/n
+              2. Develop  some techniques in computer science,/n
+              3. Understand the different area of study in computer science,/n
+              4. Navigate the career prospects in computer science./n
+              5. Conversant with computer programming concepts./n
+              6.Conversant with applications of computer science.")
+ 
+ }
